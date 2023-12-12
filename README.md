395ae17a14f7a5ccb1cdbcde575890c2764474de
guanw <guanw0826@gmail.com>
2023-12-11 20:10:37 -0500

    enable speech recognition (for browser only)

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ $npm run electron
 // run react
 $npm run start
 ```
+
+## speech recognition
+* currently it only works in real browser setup
--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  const [transcript, setTranscript] = useState('');
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onresult = (event) => {
+    const last = event.results.length - 1;
+    const speechResult = event.results[last][0].transcript;
+    setTranscript(speechResult);
+  };
+  recognition.onerror = (event) => {
+    console.error('Speech recognition error:', event.error);
+  };
+  const startRecognition = () => {
+    recognition.start();
+  };
+  const stopRecognition = () => {
+    recognition.stop();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React (Jude)
-        </a>
+        <button onClick={startRecognition}>Start Recognition</button>
+        <button onClick={stopRecognition}>Stop Recognition</button>
+        <p>{transcript}</p>
       </header>
     </div>
   );
